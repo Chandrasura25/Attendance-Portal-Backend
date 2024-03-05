595493c3c2ecd69cbcf829ebd70e10e4c7a7a71b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-03-05 19:18:07 +0100

    backend done

--- models/attendance.model.js
+++ models/attendance.model.js
@@ -2,9 +2,9 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
- date: { type: Date, required: true },
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  date: { type: Date, required: true },
   studentId: { type: String, required: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   status: { type: String, enum: ['present', 'absent'], required: true },
 });
 
