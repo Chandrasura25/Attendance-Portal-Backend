6bc0477dfc11018eca83d3dc1c452566ee03a491
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-03-05 19:18:30 +0100

    backend done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const attendanceController = require('../controllers/attendance.controller');
 const { authenticateToken } = require('../utils/authMiddleware');
 
 // Signup Endpoint
@@ -9,7 +10,17 @@ router.post('/register', userController.register);
 // Login Endpoint
 router.post('/login', userController.login);
 
-// Dashboard Endpoint (corrected)
 router.get('/dashboard', authenticateToken, userController.dashboard);
+
+// Attendance
+router.post('/attendance', authenticateToken, attendanceController.create);
+
+router.get('/attendance', authenticateToken, attendanceController.read);
+router.get('/attendance/:id', authenticateToken, attendanceController.readById);
+// Route for updating attendance record
+router.post('/attendance/:id', authenticateToken, attendanceController.update);
+
+// Route for deleting attendance record
+router.delete('/attendance/:id', authenticateToken, attendanceController.delete);
 
 module.exports = router;
